75aca6d914e967a888f7772b804b41e9f7e4477e
남궁성환 <saturn203@naver.com>
2020-10-09 13:41:17 +0900

    test 완료

--- src/main/java/com/example/vehiclefe/controller/VehicleController.java
+++ src/main/java/com/example/vehiclefe/controller/VehicleController.java
@@ -5,20 +5,31 @@ import java.util.List;
 import com.example.vehiclefe.model.Vehicle;
 import com.example.vehiclefe.service.VehicleService;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@RefreshScope
 @RestController
 @RequiredArgsConstructor
 public class VehicleController {
     private final VehicleService service;
 
+    @Value("${test.message}")
+    private String testMessage;
+
     @GetMapping("/list")
     public List<Vehicle> list(){
         List<Vehicle> vehicleList =  service.vehicleList();
         return vehicleList;
+    }
+
+    @GetMapping("/configTest")
+    public String configTest(){
+        return testMessage;
     }
 
 }
