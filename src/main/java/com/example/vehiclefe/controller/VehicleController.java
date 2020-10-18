2b78cb0a8785d9a8638543d9816e77a23a68ab09
남궁성환 <saturn203@naver.com>
2020-10-18 15:52:19 +0900

    dfsfsfdsfddfsfsdsd

--- src/main/java/com/example/vehiclefe/controller/VehicleController.java
+++ src/main/java/com/example/vehiclefe/controller/VehicleController.java
@@ -5,20 +5,35 @@ import java.util.List;
 import com.example.vehiclefe.model.Vehicle;
 import com.example.vehiclefe.service.VehicleService;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class VehicleController {
     private final VehicleService service;
 
-    @GetMapping("/list")
-    public List<Vehicle> list(){
+    
+
+    @GetMapping("/list")    
+    public String  list(Model model){
         List<Vehicle> vehicleList =  service.vehicleList();
-        return vehicleList;
+         
+        model.addAttribute("vehicleList", vehicleList);
+        return "vehicle/list.html";
+         
     }
+
+    @GetMapping("/json/list")  
+    @ResponseBody  
+    public List<Vehicle> jsonList(){
+        return   service.vehicleList();
+    }
+
+
 
 }
