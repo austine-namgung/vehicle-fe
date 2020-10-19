33d05de47dcc00013d3284730617df35bb42b21d
남궁성환 <saturn203@naver.com>
2020-10-19 10:31:41 +0900

    1e

--- src/main/java/com/example/vehiclefe/controller/VehicleController.java
+++ src/main/java/com/example/vehiclefe/controller/VehicleController.java
@@ -7,13 +7,15 @@ import com.example.vehiclefe.service.VehicleService;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
 @RefreshScope
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class VehicleController {
     private final VehicleService service;
@@ -22,12 +24,22 @@ public class VehicleController {
     private String testMessage;
 
     @GetMapping("/list")
-    public List<Vehicle> list(){
+    public String list(Model model){
+        List<Vehicle> vehicleList =  service.vehicleList();
+        model.addAttribute("vehicleList", vehicleList);
+        return "vehicle/list.html";
+        
+    }
+    @GetMapping("/json/list")  
+    @ResponseBody  
+    public List<Vehicle> jsonList(){
         List<Vehicle> vehicleList =  service.vehicleList();
         return vehicleList;
+         
     }
 
     @GetMapping("/configTest")
+    @ResponseBody
     public String configTest(){
         return testMessage;
     }
